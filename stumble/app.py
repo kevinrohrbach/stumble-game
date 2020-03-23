c9a4f13ee8742347fa29229a494626a522a5185e
Kevin Rohrbach <60136167+kevinrohrbach@users.noreply.github.com>
2020-03-23 21:33:16 +0100

    Pre. Savegame implementation commit. Navigation works

--- stumble/app.py
+++ stumble/app.py
@@ -1,15 +1,19 @@
 """Run the game."""
 
-
+from sys import exit
+from os import path
 import json
+import jsonpickle
 
+# Import classes --------------- #
 import common
 
+# load universe ---------------- #
 with open("game/elements.json") as elements:
     universe = json.load(elements)
 
-# --------------------------------------------------------
-# Starting values
+
+# Starting values -------------- #
 playerName = "Kevin"  # input('> ')
 room = "bedroom"
 
@@ -20,18 +24,40 @@ player.room = common.Room(universe, room)
 player.madness = 0
 player.inventory = []
 
+# Create save file
 
-dead = False
 
-while dead is False:
-    command = input('\n> ')
-    if command in {'N', 'E', 'S', 'W'}:
-        player_move = player.move(command)
-        if player_move is not None:
-            player.room = common.Room(universe, player_move)
-        pass
-    elif command == 'look':
-        print(player.room.descr_light)
-    else:
-        print('Invalid command.')
-    # dead = True
+# Main game --------------------- #
+def game_loop():
+    """Define main game loop."""
+    dead = False
+
+    while dead is False:
+        command = input('\n> ')
+        if command in {'N', 'E', 'S', 'W'}:
+            player_move = player.move(command)
+            if player_move is not None:
+                player.room = common.Room(universe, player_move)
+            pass
+        elif command == 'look':
+            print(player.room.descr_light)
+        else:
+            print('Invalid command.')
+    dead = True
+    exit(0)
+
+
+# Main function -------------- #
+def main():
+    """Define main function. Currently only calling game loop."""
+    # global game_state
+    # if not os.path.isfile(SAVEGAME_FILENAME):
+    #     game_state = initialize_game()
+    # else:
+    #     game_state = load_game()
+    game_loop()
+# TODO: Add load, initialise
+
+
+if __name__ == '__main__':
+    main()
