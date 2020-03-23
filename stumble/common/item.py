243ebd9209e76fd723d9bdef7766fc271e316f89
Kevin Rohrbach <60136167+kevinrohrbach@users.noreply.github.com>
2020-03-23 17:09:59 +0100

    Split classes

---
+++ stumble/common/item.py
@@ -0,0 +1,10 @@
+"""Define Item class."""
+
+
+class Item():
+    """This class represents items in the game."""
+
+    def __init__(self):
+        """Hold all fields for Item."""
+        self.name = ""  # name of the object
+        self.description = ""  # description of item to be printed
